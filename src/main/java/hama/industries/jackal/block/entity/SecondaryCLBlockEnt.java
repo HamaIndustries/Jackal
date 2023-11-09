015f6772167ea8eab49d3dbc45ad3cf65c796b13
unknown <eliatlarge@gmail.com>
2023-11-08 18:48:42 -0600

    fixed manager being hidden in secondary CL

--- src/main/java/hama/industries/jackal/block/entity/SecondaryCLBlockEnt.java
+++ src/main/java/hama/industries/jackal/block/entity/SecondaryCLBlockEnt.java
@@ -1,17 +1,13 @@
 package hama.industries.jackal.block.entity;
 
 import hama.industries.jackal.JackalMod;
-import hama.industries.jackal.logic.ICLManagerCapability;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.common.util.LazyOptional;
 
 public class SecondaryCLBlockEnt extends AbstractCLBlockEnt {
     public SecondaryCLBlockEnt(BlockPos pos, BlockState state) {
         super(JackalMod.BLOCK_ENTITIES.SECONDARY_CL, pos, state);
     }
-
-    LazyOptional<ICLManagerCapability> manager = LazyOptional.empty();
 
     @Override
     protected void registerSelf() {
